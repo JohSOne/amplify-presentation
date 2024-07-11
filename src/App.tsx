ac5ef80d0d68e10992ee5d5d7cfa78b35ff0f87e
johann.malaki <johann.malaki@softwareone.com>
2024-07-11 18:10:47 +0200

    Clear code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import '@aws-amplify/ui-react/styles.css'
 import '@fontsource/inter'
 import {HomePage} from "./components";
+import {Authenticator, Button} from "@aws-amplify/ui-react";
+import {Amplify} from "aws-amplify";
+import output from '../amplify_outputs.json'
+
+Amplify.configure(output)
+
 //import './App.css'
 
 function App() {
-
-  return (
-    <>
-        <HomePage />
-       {/* <Flex
-            width={"30%"}
-        >
-        <Card variation={"elevated"}>
-        <POICard overrides={{
-            image:{
-                src:"src/assets/mountain.png",
-            },
-            POICard:{
-                onClick:()=>alert("clicked⛰️"),
-                style:{
-                    cursor:"pointer"
-                }
-            },
-            title:{
-                children:"Pico de las Nieves"
-            },
-            aboveTitle:{
-                children:"Mountains in Gran Canaria"
-            },
-            contentText:{
-                children:"This is the top of the mountain in the middle of the Gran Canaria island. It has 1956 m" +
-                    " and offers the best view of the island. 🔭"
-            },
-            Badge:{
-                variation:"info"
-            }
-        }}/>
-        </Card>
-        </Flex>*/}
-    </>
-  )
+    return (
+        <>
+            <HomePage/>
+        </>
+    )
 }
 
 export default App
