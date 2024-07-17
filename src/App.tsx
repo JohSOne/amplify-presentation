754d881d0a1f2f77e3ab173da878ea561b345e24
johann.malaki <johann.malaki@softwareone.com>
2024-07-17 15:44:52 +0200

    Analyse types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {Amplify} from "aws-amplify";
 import './App.css'
 import React from "react";
-import HomePage from "./pages/HomePage";
 
 import output from '../amplify_outputs.json';
 import {Authenticator} from "@aws-amplify/ui-react";
