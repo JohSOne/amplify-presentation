7736ba707cb6618bff1626cf02c81a367893a38d
johann.malaki <johann.malaki@softwareone.com>
2024-07-17 20:45:24 +0200

    Fix image src

--- src/assets/mock-data.ts
+++ src/assets/mock-data.ts
@@ -4,21 +4,21 @@ export const items:iPoiCard[] = [
         aboveTitle: "Mountains",
         contentText: "This is the top of the mountain in the middle of the Gran Canaria island. It has 1956 m and" +
             " offers the best view of the island. 🔭",
-        image: "src/assets/mountain.png",
+        image: "assets/mountain.png",
         coordinates: "27.962220, -15.571574"
     },
     {
         title: "Dunas de Maspalomas",
         aboveTitle: "Desert",
         contentText: "The short Sahara. 11 km of sand dunes to walk and astonish 🏜️",
-        image: "src/assets/dunas.png",
+        image: "assets/dunas.png",
         coordinates: "27.740441, -15.581789"
     },
     {
         title: "Anfi del Mar",
         aboveTitle: "Beach",
         contentText: "An Amazing Beach with special Park/Terrace for weddings 🏖️💍",
-        image: "src/assets/anfi.png",
+        image: "assets/anfi.png",
         coordinates: "27.77301, -15.69730"
     }
 ]
